a39cbf52d0a704e32b76459c587dc6e1f9a9e282
NguyenCuong <cuongnx@infoplusvn.com>
2023-11-27 16:15:55 +0700

    # Add Upload, download API example

---
+++ UploadDownloadAPI/Controllers/FileController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UploadDownloadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost(Name = "UploadFile")]
+        [RequestSizeLimit(1024*1024*1024)]
+        public async Task<IActionResult> UploadFile(IFormFile file, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await this.WriteFile(file);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet(Name = "DownloadFile")]
+
+        public async Task<ActionResult> DownloadFile(string fileName)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data\\Files", fileName);
+
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(filePath, out var contenttype))
+            {
+                contenttype = "application/octet-stream";
+            }
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(bytes, contenttype, Path.GetFileName(filePath));
+        }
+        private async Task<string> WriteFile(IFormFile file)
+        {
+            string fileName = file.FileName;
+            try
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data\\Files");
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                var exactpath = Path.Combine(Directory.GetCurrentDirectory(), "Data\\Files", fileName);
+                using (var stream = new FileStream(exactpath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return fileName;
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message);
+            }
+            return fileName;
+        }
+    }
+}
